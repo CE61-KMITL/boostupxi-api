a14e034ee2ba73d2600db952f6283716bf965c71
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-01-15 22:32:02 +0700

    feat: setup environment and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+
+  const whitelistedDomains: string[] = configService
+    .get<string>('whitelist_domains')
+    .split(',');
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (whitelistedDomains.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    optionsSuccessStatus: 200,
+  });
+
+  await app.listen(port);
 }
 bootstrap();
