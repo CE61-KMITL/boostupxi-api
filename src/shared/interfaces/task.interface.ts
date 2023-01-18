079139697dfb1e487b3bf604ad9f8ff114c7e17f
Kittonn <tonkittipod@gmail.com>
2023-01-18 19:35:59 +0700

    feat: setup testcase interface

--- src/shared/interfaces/task.interface.ts
+++ src/shared/interfaces/task.interface.ts
@@ -1,7 +1,3 @@
 import { Document } from 'mongoose';
 
-export interface ITask extends Document {}
-
-export interface ISubmission extends Document {}
-
-export interface ITestCase extends Document {}
+export interface TaskI extends Document {}
