64e8112105329802b8b09917fdb016b9ea757624
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-02-02 14:44:10 +0700

    feat(swagger): setup swagger

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,27 +1,41 @@
-import { IsString, Min, Max, IsNumber, IsArray } from 'class-validator';
+import {
+  IsString,
+  Min,
+  Max,
+  IsNumber,
+  IsArray,
+  IsNotEmpty,
+} from 'class-validator';
 import { ITestCase } from 'src/shared/interfaces/testcase.interface';
 
 export class CreateTaskDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
 
   @IsString()
+  @IsNotEmpty()
   description: string;
 
   @IsNumber()
   @Min(1)
   @Max(5)
+  @IsNotEmpty()
   level: number;
 
   @IsArray()
+  @IsNotEmpty()
   tags: string[];
 
   @IsString()
+  @IsNotEmpty()
   hint: string;
 
   @IsArray()
+  @IsNotEmpty()
   files: string[];
 
   @IsArray()
+  @IsNotEmpty()
   testcases: ITestCase[];
 }
