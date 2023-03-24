f376dcdb3f02cad4ef7c7931f26f67f2af20195d
TeetouchQQ <teawkung@live.com>
2023-03-24 15:15:31 +0700

    chore: change AWS to local minio

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Task } from './schemas/task.schema';
