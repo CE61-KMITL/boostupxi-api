35161259cddf564e1f85e41c1e94fa666bbb0c45
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-03-30 14:37:12 +0700

    chore: update task controllers

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -22,13 +22,13 @@ import { TaskI } from 'src/shared/interfaces/task.interface';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
-@Roles(Role.AUDITOR, Role.STAFF)
-@UseGuards(JwtGuard, RolesGuard)
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @Roles(Role.AUDITOR, Role.STAFF)
+  @UseGuards(JwtGuard, RolesGuard)
   async createTask(
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: UserI,
@@ -37,16 +37,22 @@ export class TasksController {
   }
 
   @Get()
+  @Roles(Role.AUDITOR, Role.STAFF, Role.USER)
+  @UseGuards(JwtGuard, RolesGuard)
   async getTasks(): Promise<TaskI[]> {
     return await this.tasksService.getTasks();
   }
 
   @Get('/:id')
+  @Roles(Role.AUDITOR, Role.STAFF, Role.USER)
+  @UseGuards(JwtGuard, RolesGuard)
   async getTaskById(@Param('id') id: string): Promise<TaskI> {
     return await this.tasksService.getTaskById(id);
   }
 
   @Patch('/:id')
+  @Roles(Role.AUDITOR, Role.STAFF)
+  @UseGuards(JwtGuard, RolesGuard)
   async updateTask(
     @Param('id') id: string,
     @Body() updateTaskDto: UpdateTaskDto,
@@ -55,6 +61,8 @@ export class TasksController {
   }
 
   @Delete('/:id')
+  @Roles(Role.AUDITOR, Role.STAFF)
+  @UseGuards(JwtGuard, RolesGuard)
   async deleteTask(@Param('id') id: string, @GetUser() user: UserI) {
     return await this.tasksService.deleteTask(id, user);
   }
