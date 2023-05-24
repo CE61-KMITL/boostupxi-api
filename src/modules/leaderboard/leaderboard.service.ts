1aba8e1d92a093f46aadb678e4e5850a7346564b
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-05-24 19:54:27 +0700

    feat(backend): leaderboard api

--- src/modules/leaderboard/leaderboard.service.ts
+++ src/modules/leaderboard/leaderboard.service.ts
@@ -3,17 +3,62 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../users/schemas/user.schema';
 import { Model } from 'mongoose';
 import { IUser } from '@/common/interfaces/user.interface';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class LeaderboardService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<IUser>,
   ) {}
-  getLeaderboard() {
-    return 'This action returns all leaderboard';
+
+  async getLeaderboard(page = 1, limit = 20) {
+    const users = await this.userModel.aggregate([
+      {
+        $match: { role: 'user' },
+      },
+      {
+        $sort: { score: -1 },
+      },
+      {
+        $project: {
+          password: 0,
+          role: 0,
+        },
+      },
+      {
+        $skip: (page - 1) * limit,
+      },
+      {
+        $limit: limit,
+      },
+    ]);
+
+    const count = await this.userModel.find({ role: 'user' }).countDocuments();
+    const pages = Math.ceil(count / limit);
+
+    return {
+      currentPage: page,
+      pages,
+      data: await Promise.all(users),
+    };
   }
 
-  getLeaderboardByGroup() {
-    return 'This action returns leaderboard by group';
+  async getLeaderboardByGroup() {
+    const userGroups = await this.userModel.aggregate([
+      {
+        $match: { role: 'user' },
+      },
+      {
+        $group: {
+          _id: '$group',
+          score: { $sum: '$score' },
+        },
+      },
+      {
+        $sort: { score: -1 },
+      },
+    ]);
+
+    return userGroups;
   }
 }
