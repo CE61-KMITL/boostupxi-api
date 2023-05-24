2a652359fe7d760fe57a9a3579682c52873d59ef
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-05-24 19:51:51 +0700

    feat(backend): leaderboard api

--- src/modules/leaderboard/leaderboard.controller.ts
+++ src/modules/leaderboard/leaderboard.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { LeaderboardService } from './leaderboard.service';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from '@/common/guards/jwt.guard';
@@ -10,8 +17,23 @@ export class LeaderboardController {
   constructor(private readonly leaderboardService: LeaderboardService) {}
 
   @Get('/')
-  async getLeaderboard() {
-    return await this.leaderboardService.getLeaderboard();
+  async getLeaderboard(
+    @Query(
+      'page',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    page: number,
+    @Query(
+      'limit',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    limit: number,
+  ) {
+    return await this.leaderboardService.getLeaderboard(page, limit);
   }
 
   @Get('/group')
